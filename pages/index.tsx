103699d3632319c129ba6bd3f7ad9144ab55c857
serial-kim <serial@worksmobile.com>
2021-02-01 01:14:51 +0900

    dev(setup): add absolute import config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '~/styles/Home.module.css';
 
 const Home: React.FC = () => {
   const [, setText] = useState<string>();
