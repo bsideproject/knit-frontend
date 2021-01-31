60e215b2786b1f8aa0a0248742a305d5f3f6bdff
serial-kim <serial@worksmobile.com>
2021-01-31 22:08:37 +0900

    dev(lint): fix exist files to match lint rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
