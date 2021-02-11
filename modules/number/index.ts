83f8c2b2d524f259a9fa8b6c5f7e8e1260f4c9c6
serial-kim <serial@worksmobile.com>
2021-02-11 22:34:28 +0900

    Revert "module: add number/addCommas"
    This reverts commit f153e4a2c6a8f20881ad9521921fbd0aeff5df83.

--- modules/number/index.ts
+++
@@ -1,4 +0,0 @@
-export function addCommas(number: number | string) {
-  if (typeof number !== 'number' && typeof number !== 'string') return number;
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
