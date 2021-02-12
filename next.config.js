748ab000ac34ceebdc75c41d17317bc2969d3980
serial-kim <serial@worksmobile.com>
2021-02-12 12:33:58 +0900

    styles: ass sass absolute path config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-
-
 const _ = require('lodash');
-
+const path = require('path');
 
 ['NEXT_PUBLIC_BASE_URL'].forEach((key) => {
-    if (!_.has(process.env, key)) {
-      console.error(`${key} is not exist on process environment`);
-      process.exit(1);
-    }
-  });
-
-  module.exports = {
-    reactStrictMode: true,
+  if (!_.has(process.env, key)) {
+    console.error(`${key} is not exist on process environment`);
+    process.exit(1);
   }
+});
+
+module.exports = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
