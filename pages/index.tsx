6a76eb6092dff49b7660bc35fa42709ab524f812
kwonth211 <kwonth211@naver.com>
2021-02-16 00:24:46 +0900

    [Fix] reorganize component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,114 +1,17 @@
 import classNames from 'classnames';
-import { MostViewData } from '~/@types/MostView';
 import { Container } from '~/atoms/layout';
 import { MostView } from '~/molecules/preview';
 import { Banner } from '~/molecules/banner';
 import styles from './index.module.scss';
-import { Typograpy } from '~/atoms/typography';
 import { Featured } from '~/molecules/featured';
-
-const mockData = {
-  mostView: [
-    {
-      id: 1,
-      title:
-        '여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...',
-      desc:
-        '여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...',
-      viewCount: 9999,
-      likeCount: 9999,
-    },
-    {
-      id: 2,
-      title:
-        '여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...',
-      desc:
-        '여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...',
-      viewCount: 9999,
-      likeCount: 9999,
-    },
-    {
-      id: 3,
-      title:
-        '여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...',
-      desc:
-        '여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...',
-      viewCount: 9999,
-      likeCount: 9999,
-    },
-    {
-      id: 4,
-      title:
-        '여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...',
-      desc:
-        '여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...',
-      viewCount: 9999,
-      likeCount: 9999,
-    },
-    {
-      id: 5,
-      title:
-        '여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...',
-      desc:
-        '여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...',
-      viewCount: 9999,
-      likeCount: 9999,
-    },
-    {
-      id: 6,
-      title:
-        '여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...여기에 타이틀. 최근 많이 조회된 문서가 이자리 넘어가면 말줄임밑줄임...',
-      desc:
-        '여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...여기에 내용이 들어감 여기 내용을 블라블라 써내려감 넘어가면 말줄임말줄임...',
-      viewCount: 9999,
-      likeCount: 9999,
-    },
-  ] as ReadonlyArray<MostViewData>,
-};
 
 export default function MainPage() {
   return (
     <>
       <Banner />
       <Container className={styles.main}>
-        <div style={{ display: 'flex' }}>
-          <div className={classNames(styles.column, styles.left)}>
-            <div className={styles.section}>
-              <div className={styles.titleCover}>
-                <Typograpy>최근 많이 조회된 문서</Typograpy>
-              </div>
-              <div className={styles.contents}>
-                <div className={styles.sectionColumn}>
-                  {mockData.mostView
-                    .filter((_, index) => index % 2 === 0)
-                    .map(({ id, title, desc, viewCount, likeCount }, index) => (
-                      <MostView
-                        key={id}
-                        number={index * 2 + 1}
-                        title={title}
-                        desc={desc}
-                        viewCount={viewCount}
-                        likeCount={likeCount}
-                      />
-                    ))}
-                </div>
-                <div className={styles.sectionColumn}>
-                  {mockData.mostView
-                    .filter((_, index) => index % 2 === 1)
-                    .map(({ id, title, desc, viewCount, likeCount }, index) => (
-                      <MostView
-                        key={id}
-                        number={index * 2 + 2}
-                        title={title}
-                        desc={desc}
-                        viewCount={viewCount}
-                        likeCount={likeCount}
-                      />
-                    ))}
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className={styles.mostViewWrapper}>
+          <MostView styles={styles} />
           <div className={classNames(styles.column, styles.right)}>
             가이드 배너 들어가야하는 위치
           </div>
