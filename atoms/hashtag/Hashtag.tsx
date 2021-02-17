576bb33941318e610b3a70a83e20e2a89377c1be
kwonth211 <kwonth211@naver.com>
2021-02-17 09:30:14 +0900

    change hashtag props

--- atoms/hashtag/Hashtag.tsx
+++ atoms/hashtag/Hashtag.tsx
@@ -2,10 +2,7 @@ import Link from 'next/link';
 import styles from './Hashtag.module.scss';
 import { Hashtag as HashtagType } from '~/@types/Hashtag';
 
-interface HashtagProps extends HashtagType {
-  url: string;
-  title: string;
-}
+type HashtagProps = HashtagType;
 
 export default function Hashtag({ url, title }: HashtagProps) {
   return (
