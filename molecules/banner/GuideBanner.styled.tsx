495a5d3eaebf2811a57f90a8fe50dad0ae7806ef
serial-kim <serial@worksmobile.com>
2021-02-20 18:30:46 +0900

    refactor: GuideBanner 내 import 구문 변경

--- molecules/banner/GuideBanner.styled.tsx
+++ molecules/banner/GuideBanner.styled.tsx
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
-import * as SideBanner from '~/atoms/banner/SideBanner';
+import { Container as SideBannerContainer } from '~/atoms/banner/SideBanner';
 
-export const Container = styled(SideBanner.Container)`
+export { Title, Desc } from '~/atoms/banner/SideBanner';
+
+export const Container = styled(SideBannerContainer)`
   background: rgba(129, 0, 127, 0.08);
 `;
-
-export const { Title, Desc } = SideBanner;
