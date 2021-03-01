6664fbc2dc2df4b0706811a4bd505422ede26042
serial-kim <serial@worksmobile.com>
2021-03-01 19:50:28 +0900

    useEffect 분리

--- molecules/signin/NaverSigninButton.tsx
+++ molecules/signin/NaverSigninButton.tsx
@@ -4,18 +4,22 @@ import { injectSigninSDK } from '~/utils/auth';
 
 const NaverSigninButton: FC = () => {
   useEffect(() => {
-    injectSigninSDK(SigninType.NAVER).then(() => {
-      const naverLogin = new (window as any).naver.LoginWithNaverId({
-        clientId: process.env.NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER,
-        callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/signin/callback/${SigninType.NAVER}`,
-        isPopup: false,
-        loginButton: { color: 'white', type: 3, height: 50 },
-      });
-      naverLogin.init();
-    });
+    injectNaverSigninSDK();
   }, []);
 
   return <div id="naverIdLogin" />;
 };
 
 export default NaverSigninButton;
+
+async function injectNaverSigninSDK() {
+  await injectSigninSDK(SigninType.NAVER);
+
+  const naverLogin = new (window as any).naver.LoginWithNaverId({
+    clientId: process.env.NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER,
+    callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/signin/callback/${SigninType.NAVER}`,
+    isPopup: false,
+    loginButton: { color: 'white', type: 3, height: 50 },
+  });
+  naverLogin.init();
+}
