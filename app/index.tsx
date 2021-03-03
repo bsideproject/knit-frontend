846069481d3942f3781813a14e5e0ae69601777c
kwonth211 <kwonth211@naver.com>
2021-03-03 20:48:50 +0900

    rollback env

--- app/index.tsx
+++ app/index.tsx
@@ -2,22 +2,20 @@ import type { AppProps } from 'next/app';
 import { useEffect, VFC } from 'react';
 import { wrapper } from './store';
 import { Layout, Header, Footer } from '~/atoms/layout';
-import axios from '~/utils/api';
+// import axios from '~/utils/api';
 
 const App: VFC<AppProps> = ({ Component, pageProps, router }) => {
   const { pathname } = router;
 
-  useEffect(() => {
-    testApi();
-  }, []);
+  useEffect(() => {}, []);
 
-  const testApi = () => {
-    const response = axios.get(`/`);
+  // const testApi = () => {
+  //   const response = axios.get(`/`);
 
-    response.then(() => {
-      console.log('ok!');
-    });
-  };
+  //   response.then(() => {
+  //     console.log('ok!');
+  //   });
+  // };
 
   if (pathname.startsWith('/signin')) {
     return <Component />;
--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const _ = require('lodash');
+// const _ = require('lodash');
 const path = require('path');
 
 // temp comment 처리
-['NEXT_PUBLIC_BASE_URL', 'NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER', 'NEXT_PUBLIC_API_URL'].forEach(
-  (key) => {
-    if (!_.has(process.env, key)) {
-      console.error(`${key} is not exist on process environment`);
-      process.exit(1);
-    }
-  }
-);
+// ['NEXT_PUBLIC_BASE_URL', 'NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER', 'NEXT_PUBLIC_API_URL'].forEach(
+//   (key) => {
+//     if (!_.has(process.env, key)) {
+//       console.error(`${key} is not exist on process environment`);
+//       process.exit(1);
+//     }
+//   }
+// );
 
 module.exports = {
   reactStrictMode: true,
