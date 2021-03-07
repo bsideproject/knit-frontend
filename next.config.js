7495d31942e6f614fa8b9b85a8fd0952efcc8b3b
kwonth211 <kwonth211@naver.com>
2021-03-07 17:29:30 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
+const _ = require('lodash');
 const path = require('path');
 
 ['NEXT_PUBLIC_BASE_URL', 'NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER', 'NEXT_PUBLIC_API_URL'].forEach(
   (key) => {
-    if (!process.env[key]) {
-      console.error('value >>', process.env[key]);
+    if (!_.has(process.env, key)) {
       console.error(`${key} is not exist on process environment`);
       process.exit(1);
     }
