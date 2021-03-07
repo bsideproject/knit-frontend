0adc343f9e2f21f7157f0a9d10119a4e0c3366f1
kwonth211 <kwonth211@naver.com>
2021-03-07 17:12:16 +0900

    something wrong..

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 ['NEXT_PUBLIC_BASE_URL', 'NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER', 'NEXT_PUBLIC_API_URL'].forEach(
   (key) => {
     if (!process.env[key]) {
+      console.error(process.env[key]);
       console.error(`${key} is not exist on process environment`);
       process.exit(1);
     }
