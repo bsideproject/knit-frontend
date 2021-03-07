676b707ca8b352465730b42c40fce69592ebd19e
kwonth211 <kwonth211@naver.com>
2021-03-07 19:25:57 +0900

    change env

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// const _ = require('lodash');
+const _ = require('lodash');
 const path = require('path');
 
 ['NEXT_PUBLIC_BASE_URL', 'NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER', 'NEXT_PUBLIC_API_URL'].forEach(
   (key) => {
-    // if (!_.has(process.env, key)) {
-    if (!process.env[key]) {
-      console.error(`${key} is not exist on process environment`);
-      process.exit(1);
+    if (!_.has(process.env, key)) {
+      // if (!process.env[key]) {
+      //   console.error(`${key} is not exist on process environment`);
+      //   process.exit(1);
     }
   }
 );
