34a6b8a2da304ad037dd0792eda6bd8b0fd923ca
kwonth211 <kwonth211@naver.com>
2021-03-09 18:05:46 +0900

    add setup

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,6 @@ FROM node:14.16.0
 
 WORKDIR /usr/src/app
 
-
-ENV NEXT_PUBLIC_BASE_URL=${NEXT_PUBLIC_BASE_URL}
-
 # Install dependencies
 COPY package*.json ./
 RUN yarn
--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 const _ = require('lodash');
 const path = require('path');
 
-const Dotenv = require('dotenv');
-Dotenv.config();
-['NEXT_PUBLIC_BASE_URL', 'NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER', 'NEXT_PUBLIC_API_URL'].forEach(
-  (key) => {
-    if (!_.has(process.env, key)) {
-      // if (!process.env[key]) {
+// const Dotenv = require('dotenv');
+// Dotenv.config();
 
-      console.error(`${key} is not exist on process environment`);
-      //   process.exit(1);
-    }
+const ENV_KEY_LIST = [
+  'NEXT_PUBLIC_BASE_URL',
+  'NEXT_PUBLIC_API_URL',
+  'NEXT_PUBLIC_AUTH_CLIENT_ID_GOOGLE',
+  'NEXT_PUBLIC_AUTH_CLIENT_ID_NAVER',
+];
+ENV_KEY_LIST.forEach((key) => {
+  if (!_.has(process.env, key)) {
+    console.error(`${key} is not exist on process environment`);
+    process.exit(1);
   }
-);
+});
 
 module.exports = {
   reactStrictMode: true,
