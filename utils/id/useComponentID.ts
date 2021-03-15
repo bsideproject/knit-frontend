84f16cc174db8241a4df74640d2e1f4c2fb41303
serial-kim <serial@worksmobile.com>
2021-03-15 22:59:07 +0900

    function keyword -> arrow function

--- utils/id/useComponentID.ts
+++ utils/id/useComponentID.ts
@@ -2,16 +2,18 @@ import { useState } from 'react';
 
 let id = 1;
 
-function generateId() {
+const generateId = () => {
   if (id === Number.MAX_SAFE_INTEGER) {
     id = 0;
   } else {
     id += 1;
   }
   return id;
-}
+};
 
-export default function useComponentId() {
+const useComponentId = () => {
   const [componentId] = useState(() => `component-id-${generateId()}`);
   return componentId;
-}
+};
+
+export default useComponentId;
